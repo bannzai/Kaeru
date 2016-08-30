e68603f2c7ff653dbaa3cc51412d9acfa556a8a4
--global <hirose.yudai@moneyforward.co.jp>
2016-08-30 18:31:13 +0900

    update demo

---
+++ KaeruDemo/KaeruDemo/DetailViewController.swift
@@ -0,0 +1,94 @@
+//
+//  DetailViewController.swift
+//  KaeruDemo
+//
+//  Created by Hirose.Yudai on 2016/08/30.
+//  Copyright © 2016年 kingkong999yhirose. All rights reserved.
+//
+
+import UIKit
+
+internal extension UIImage {
+    func blur(radius: CGFloat) -> UIImage {
+        guard let cgImage = CGImage,
+            filteredImage = Optional(CoreImage.CIImage(CGImage: cgImage)),
+            blurFilter = CIFilter(name: "CIGaussianBlur")
+            else {
+                return self
+        }
+        
+        blurFilter.setDefaults()
+        blurFilter.setValue(filteredImage, forKey: kCIInputImageKey)
+        blurFilter.setValue(radius, forKey: kCIInputRadiusKey)
+        
+        guard let cropFilter = CIFilter(name: "CICrop") else {
+            return self
+        }
+        
+        cropFilter.setValue(blurFilter.outputImage, forKey: kCIInputImageKey)
+        cropFilter.setValue(CIVector(CGRect: filteredImage.extent), forKey: "inputRectangle")
+        
+        guard let outputImage = cropFilter.outputImage else {
+            return self
+        }
+        
+        return UIImage(CIImage: outputImage)		
+    }		
+}		
+
+
+
+
+class DetailViewController: UIViewController {
+    class func instance() -> DetailViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewControllerWithIdentifier("DetailViewController") as! DetailViewController
+        return viewController
+    }
+    
+    var image: UIImage?
+    var blurImage: UIImage?
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.contentMode = .ScaleAspectFill
+        imageView.image = image
+        
+        label.text = [
+            "Now is \(navigationController!.viewControllers.count)th page.",
+            "You can press [ShowList] or [ShowHistory] buttons",
+            "When [ShowList] pressed, push to List ViewController ",
+            "When [ShowHistory] pressed, see the history of snapshot about ViewController from UINavigationController.viewControllers stack.",
+            "And you can tap ViewController, when close history and return in choose ViewController.",
+            ]
+            .reduce("")
+            { $0 + $1 + "\n" }
+        
+        setupBlurImagesInBackground()
+    }
+    
+    
+    private func setupBlurImagesInBackground() {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            let blurImage = self.image?.blur(10)
+            dispatch_async(dispatch_get_main_queue()) {
+                self.blurImage = blurImage
+            }
+        }
+    }
+    
+    @IBAction func showHIstoryPressed(sender: AnyObject) {
+        guard let blurImage = blurImage else {
+            navigationController?.presentHistory()
+            return
+        }
+        let imageView = UIImageView(image: blurImage)
+        imageView.clipsToBounds = true
+        navigationController?.presentHistory(imageView)
+    }
+    
+}
--- KaeruDemo/KaeruDemo/ViewController.swift
+++ KaeruDemo/KaeruDemo/ViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class Cell: UITableViewCell {
+    @IBOutlet weak var contentImageView: UIImageView!
+    
+    final func setup(with image: UIImage) {
+        contentImageView.image = image
+    }
+}
+
+final class ViewController: UITableViewController {
     class func instance() -> UIViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewControllerWithIdentifier("ViewController") as! ViewController
@@ -17,35 +25,60 @@ final class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
 
-    private let colors: [UIColor] = [
-        .grayColor(),
-        .whiteColor(),
-        .redColor(),
-        .blueColor(),
-        .greenColor(),
-        .yellowColor(),
-        .magentaColor(),
-        .purpleColor(),
+    private let images: [UIImage] = [
+        UIImage(named: "image1")!,
+        UIImage(named: "image2")!,
+        UIImage(named: "image3")!,
+        UIImage(named: "image4")!,
+        UIImage(named: "image5")!,
+        UIImage(named: "image6")!,
         ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.navigationBar.tintColor = .blackColor()
-        navigationController?.navigationBar.barTintColor = .orangeColor()
+        navigationController?.navigationBar.barTintColor = .whiteColor()
         
-        if let count = navigationController?.viewControllers.count {
-            view.backgroundColor = colors[count - 1]
-            label.text = "viewController: \(count - 1)"
+        tableView.registerNib(Cell.nib(), forCellReuseIdentifier: Cell.className)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        guard let image = sender as? UIImage,
+            viewController = segue.destinationViewController as? DetailViewController
+            else {
+            return
         }
+        
+        viewController.image = image
+    }
+}
+
+extension ViewController {
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
     
-    @IBAction func showViewerButtonPressed(sender: AnyObject) {
-        navigationController?.presentHistory()
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
     }
     
-    @IBAction func pushButtonPressed(sender: AnyObject) {
-        navigationController?.pushViewController(ViewController.instance(), animated: true)
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 240
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(Cell.className, forIndexPath: indexPath) as! Cell
+        cell.setup(with: images[indexPath.row])
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("toDetail", sender: images[indexPath.row])
     }
 }
 
